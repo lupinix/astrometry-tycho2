1404059aee34b6a0133a90abd3a404276e153c7d
Christian Dersch <lupinix@mailbox.org>
2016-09-27 14:03:09 +0200

    Imported Tycho-2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # astrometry-tycho2
-Tycho-2 catalogue for astrometry.net
+Tycho-2 catalogue for astrometry.net, same content as Tycho-2 shipped by astrometry.net itself, but generated from source. Useful for Linux distribution packaging.
---
+++ cat/download-tycho2-from-vizier.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+FTPURL="ftp://cdsarc.u-strasbg.fr/pub/cats/I/259"
+
+# Get index files and docs
+wget ${FTPURL}/guide.pdf
+wget ${FTPURL}/ReadMe
+wget ${FTPURL}/index.dat.gz
+wget ${FTPURL}/suppl_1.dat.gz
+wget ${FTPURL}/suppl_2.dat.gz
+wget ${FTPURL}/tyc2.dat.00.gz
+wget ${FTPURL}/tyc2.dat.01.gz
+wget ${FTPURL}/tyc2.dat.02.gz
+wget ${FTPURL}/tyc2.dat.03.gz
+wget ${FTPURL}/tyc2.dat.04.gz
+wget ${FTPURL}/tyc2.dat.05.gz
+wget ${FTPURL}/tyc2.dat.06.gz
+wget ${FTPURL}/tyc2.dat.07.gz
+wget ${FTPURL}/tyc2.dat.08.gz
+wget ${FTPURL}/tyc2.dat.09.gz
+wget ${FTPURL}/tyc2.dat.10.gz
+wget ${FTPURL}/tyc2.dat.11.gz
+wget ${FTPURL}/tyc2.dat.12.gz
+wget ${FTPURL}/tyc2.dat.13.gz
+wget ${FTPURL}/tyc2.dat.14.gz
+wget ${FTPURL}/tyc2.dat.15.gz
+wget ${FTPURL}/tyc2.dat.16.gz
+wget ${FTPURL}/tyc2.dat.17.gz
+wget ${FTPURL}/tyc2.dat.18.gz
+wget ${FTPURL}/tyc2.dat.19.gz
+
+echo "Downloaded Tycho2 catalogue"
