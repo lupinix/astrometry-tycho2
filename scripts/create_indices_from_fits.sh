a9b4765d9c3ef1fc5719abc2ec7d077af9ad4269
Christian Dersch <lupinix@mailbox.org>
2017-10-16 16:58:25 +0200

    Name indices in the same way as upstream

--- scripts/create_indices_from_fits.sh
+++ scripts/create_indices_from_fits.sh
@@ -11,11 +11,13 @@ set -e
 # Generate indices as mentioned in http://data.astrometry.net/4100/README
 cd build
 for i in `seq -w 11 19` ; do
-    build-astrometry-index -d 3 -o index-tycho2-${i}.fits -P $i -S MAG_VT -B 0.1 -s 0 -r 1 -I 41${i} -M -i tyc2.fits
+    build-astrometry-index -d 3 -o index-41${i}.fits -P $i -S MAG_VT -B 0.1 -s 0 -r 1 -I 41${i} -M -i tyc2.fits
 done
 
-for i in `seq -w 7 10` ; do
-    build-astrometry-index -d 4 -o index-tycho2-${i}.fits -P $i -S MAG_VT -B 0.1 -s 0 -r 1 -I 41${i} -M -i tyc2.fits
+build-astrometry-index -d 4 -o index-4110.fits -P 10 -S MAG_VT -B 0.1 -s 0 -r 1 -I 4110 -M -i tyc2.fits
+
+for i in `seq -w 7 9` ; do
+    build-astrometry-index -d 4 -o index-410${i}.fits -P $i -S MAG_VT -B 0.1 -s 0 -r 1 -I 410${i} -M -i tyc2.fits
 done
 
 cd ..
